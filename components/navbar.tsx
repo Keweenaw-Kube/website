5cbcb6c6ddcf392c36a2012f5cecdb22d9e2068b
Max Isom <hi@maxisom.me>
2020-12-12 18:44:59 -0500

    Add Discord server link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import {Navbar, Button, Tag} from 'rbx';
+import {Navbar, Button, Icon} from 'rbx';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faDiscord} from '@fortawesome/free-brands-svg-icons';
 import LoginButton from './login-button';
 import {useAPI} from './api-client-context';
 
@@ -35,6 +37,12 @@ const MainNavbar = () => {
 					<Link passHref href="/about">
 						<Navbar.Item>About</Navbar.Item>
 					</Link>
+
+					<Navbar.Item href="https://discord.gg/MNxSwZ45c4" title="Join our Discord server!">
+						<Icon>
+							<FontAwesomeIcon icon={faDiscord}/>
+						</Icon>
+					</Navbar.Item>
 				</Navbar.Segment>
 
 				<Navbar.Segment align="end">
